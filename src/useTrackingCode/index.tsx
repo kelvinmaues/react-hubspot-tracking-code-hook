b7a7297c2e09d0e37da10e1c4055777c20aa0564
Kelvin Gaia Maués <kgmdeveloper@gmail.com>
2021-07-16 10:48:21 -0300

    new: verion and improvement

--- src/useTrackingCode/index.tsx
+++ src/useTrackingCode/index.tsx
@@ -1,10 +1,10 @@
-import { IMethodParam, IPropsUseSetTrackEvent } from "../models";
+import { IMethodParam, IPropsUseSetTrackEvent } from '../models'
 
-export type IPushParams = [IMethodParam, (string | object)?];
+export type IPushParams = [IMethodParam, (string | object)?]
 
 declare global {
   interface Window {
-    _hsq: { push: (callParam: IPushParams) => void };
+    _hsq: { push: (callParam: IPushParams) => void }
   }
 }
 
@@ -12,52 +12,58 @@ declare global {
  * The shape of the hook
  */
 export type IUseTrackingCode = {
-  setContentType: (contentType: string) => void;
-  setPathPageView: (path: string) => void;
-  setTrackPageView: () => void;
-  setIdentity: (email: string, customPropertities?: {}) => void;
-  setTrackEvent: ({ eventId, value }: IPropsUseSetTrackEvent) => void;
-};
+  setContentType: (contentType: string) => void
+  setPathPageView: (path: string) => void
+  setTrackPageView: () => void
+  setIdentity: (email: string, customPropertities?: {}) => void
+  setTrackEvent: ({ eventId, value }: IPropsUseSetTrackEvent) => void
+}
 
 const useTrackingCode = (): IUseTrackingCode => {
-  const _hsq = typeof window !== 'undefined' && window._hsq ? window._hsq : [];
+  const _hsq = typeof window !== 'undefined' && window._hsq ? window._hsq : []
 
   const setContentType = (contentType: string): void => {
-    _hsq.push(["setContentType", contentType]);
-  };
+    _hsq.push(['setContentType', contentType])
+  }
 
   const setTrackPageView = () => {
-    _hsq.push(["trackPageView"]);
-  };
+    _hsq.push(['trackPageView'])
+  }
 
   const setPathPageView = (path: string): void => {
     // This function updates the path
-    _hsq.push(["setPath", path]);
+    _hsq.push(['setPath', path])
     // This function track the current updated page path
-    setTrackPageView();
-  };
+    setTrackPageView()
+  }
 
   const setIdentity = (email: string, customPropertities?: {}) => {
     _hsq.push([
-      "identify",
+      'identify',
       {
         email,
-        ...customPropertities,
-      },
-    ]);
-  };
+        ...customPropertities
+      }
+    ])
+  }
 
   const setTrackEvent = ({ eventId, value }: IPropsUseSetTrackEvent) => {
     _hsq.push([
-      "trackEvent",
+      'trackEvent',
       {
         id: eventId,
-        value,
-      },
-    ]);
-  };
+        value
+      }
+    ])
+  }
 
-  return { setContentType, setPathPageView, setTrackPageView, setIdentity, setTrackEvent };
-};
+  return {
+    setContentType,
+    setPathPageView,
+    setTrackPageView,
+    setIdentity,
+    setTrackEvent
+  }
+}
 
-export default useTrackingCode;
+export default useTrackingCode
